9d19dd326d6a593a34a2ab78d99e6b4c3d24ed9e
Igor Casciotti <ic.cspec@protonmail.com>
2024-08-13 16:50:19 +0200

    Update gmhs.go

--- gmhs.go
+++ gmhs.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io/fs"
+	"net"
 	"net/http"
 	"os"
 	"path"
@@ -23,6 +24,40 @@ func defineFlags() {
 	flag.StringVar(&upload, "upload", upload, "upload folder")
 
 	flag.Parse()
+}
+
+func validateShareDir() error {
+	if dir == "current directory" {
+		dir, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Error getting current directory")
+			return err
+		}
+		fmt.Printf("Serving files from current directory: %s\n", dir)
+	} else {
+		if _, err := os.Stat(dir); errors.Is(err, fs.ErrNotExist) {
+			fmt.Println("Error Validating Share Directory")
+			fmt.Printf("Directory %s does not exist\n", dir)
+			return err
+		}
+		fmt.Printf("Serving files from directory: %s\n", dir)
+	}
+	return nil
+}
+
+func validateUploadDir() error {
+	if _, err := os.Stat(upload); errors.Is(err, fs.ErrNotExist) {
+		fmt.Printf("Upload directory '%s' does not exist... creating it\n", upload)
+		err := os.Mkdir(upload, 0222)
+		if err != nil {
+			fmt.Printf("Error creating directory %s\n", upload)
+			return err
+		}
+
+	} else {
+		fmt.Printf("Upload directory at: %s\n", filepath.Dir(upload))
+	}
+	return nil
 }
 
 func validateDirectories() error {
@@ -55,9 +90,26 @@ func validateDirectories() error {
 	return nil
 }
 
+func grabIP() (string, error) {
+	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: net.ParseIP("1.1.1.1"), Port: 53})
+	if err != nil {
+		fmt.Println("Error grabbing IP")
+		return "", err
+	}
+	ip := conn.LocalAddr().(*net.UDPAddr).IP.String()
+	conn.Close()
+	return ip, nil
+}
 func main() {
 	defineFlags()
-	err := validateDirectories()
+
+	err := validateShareDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = validateUploadDir()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -67,6 +119,12 @@ func main() {
 
 	mux.HandleFunc("/", http.FileServer(http.Dir(dir)).ServeHTTP)
 
-	fmt.Printf("Listening on: %s\n", "http://localhost:"+port)
+	ip, err := grabIP()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("upload directory at URL: %s\n", path.Join("localhost:"+port, filepath.Base(upload)))
+
+	fmt.Printf("Listening on: %s:%s\n", ip, port)
 	http.ListenAndServe(":"+port, mux)
 }
